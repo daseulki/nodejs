d14c9c74195b03839325dc0dceff44a5c3be4764
daseulki <sisial1002@gmail.com>
2018-11-23 17:17:37 +0900

    express start

---
+++ package/myPackage.js
@@ -0,0 +1,15 @@
+// Finally I got vs code..
+// npm init
+/*
+{
+  "name": "mpackage25552",
+  "version": "1.0.0",
+  "description": "test package",
+  "main": "myPackage.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "daseulki",
+  "license": "MIT"
+}
+*/
