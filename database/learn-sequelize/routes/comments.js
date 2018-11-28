e86a393120cec5c95932f56c0bc5b6f2a3650f44
daseulki <sisial1002@gmail.com>
2018-11-28 18:06:17 +0900

    edit routes (..ing)

---
+++ database/learn-sequelize/routes/comments.js
@@ -0,0 +1,47 @@
+const express= require('express');
+const router = express.Router();
+
+router.get('/:id',(req,res,next)=>{
+    Comment.findAll({
+        include: {
+            model: getUser,
+            where: { id: req.params.id },
+        }
+    })  
+    .then((comments) => {
+      res.json(comments);
+    })
+    .catch((err) => {
+      console.error(err);
+      next(err);
+    })
+})
+
+router.patch('/:id',(req,res,next) => {
+    Comment.update({
+        comment : req.body.comment,
+    }, {
+        where: { id, req.params.id },
+    })
+});
+
+router.delete('/:id',(req,res,next) => {
+    Comment.destroy()
+});
+
+router.post('/',(req,res,next) => {
+    Comment.create({
+        commenter: req.body.id,
+        comment: req.body.comment,
+    })
+    .then((comments) => {
+        res.json(comments);
+      })
+      .catch((err) => {
+        console.error(err);
+        next(err);
+      })
+});
+
+
+module.exports = router;
--- database/learn-sequelize/routes/users.js
+++ database/learn-sequelize/routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var { User } = require('../models')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//얘네 다 promise여서 then으로.. 성공한 경우와 실패한 경우 모두 써야함 
+router.get('/', (req, res, next) => {
+  User.findAll()  //db에서 다 갖고와서 배열로 .. 
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      next(err);
+    })
+});
+
+router.post('/', (req, res, next) => {
+  User.create({
+    name: req.body.name,
+    age: req.body.age,
+    married: req.body.married,
+  })
+    .then((result) => { // post요청이 성공했을때는 보통 201 사용.. 
+      console.log(result);
+      res.status(201).json(result) //client한테 보내는 부분 
+    })
+    .catch((err) => {
+      console.error(err);
+      next(err);
+    })
 });
 
 module.exports = router;
